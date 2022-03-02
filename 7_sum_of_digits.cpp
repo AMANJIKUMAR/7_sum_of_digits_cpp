326e0d2a464f70bf47b86ba6c79a4cbac4a89b7d
Aman kumar <72993406+AMANJIKUMAR@users.noreply.github.com>
2022-03-02 15:40:27 +0530

    Update 7_sum_of_digits.cpp

--- 7_sum_of_digits.cpp
+++ 7_sum_of_digits.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main() {
     int n;
-    cin>>n;
+    cin>>n;                               //input from user
 
-    int sum=0;
+    int sum=0;                            //initially take sum =0
     while (n!=0){
-        sum = sum + n%10 ;
-        n = n/10;
+        sum = sum + n%10 ;                 // % used for taking remainder value for single sum
+        n = n/10;                          // / is used for eleminate last digit value
     }
     cout<<"sum of digit:"<<sum;
     cout<<endl;
